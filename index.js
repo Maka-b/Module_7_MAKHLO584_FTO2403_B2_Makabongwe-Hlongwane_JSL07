5b7b69fe1f9ef0dbb8292403df9fc5b17319f9a7
Makabongwe Hlongwane <jabumak001@gmail.com>
2024-06-03 19:53:26 +0200

    debugged and eventlistener added

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput =document.getElementById('personalMessage');
+      const courseNameInput = document.getElementById('courseName'); 
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -25,8 +25,15 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
+      certificateContent.innerHTML = `
+      <h2> Certificate of Achievement </h2>
+      <p>This is to certify that<p>
       <h3>${studentName}</h3>
+      <p> has almost completed the <p>
+      <h3>${courseName}</h3>
+      <p> With legendary perserverance and world-class bad-assery for never giving up 🏆  </p>
+      <div><img src = './logo.png' width="500"></div>
+      <h3>${personalMessage}</h3>
     `;
     
       //  Display the modal
@@ -39,7 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
+    closeModal.addEventListener('click', function () {
+      modal.style.display = 'none'
       
     });
   });
